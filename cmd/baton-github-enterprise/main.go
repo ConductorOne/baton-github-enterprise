358b2c23d831b09092375e7fbdf41d133adca47d
subencheng <ben.su@conductorone.com>
2026-02-12 11:40:49 -0800

    add defaultCapabilityBuilder

--- cmd/baton-github-enterprise/main.go
+++ cmd/baton-github-enterprise/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	cfg "github.com/conductorone/baton-github-enterprise/pkg/config"
+	githubConnector "github.com/conductorone/baton-github/pkg/connector"
 	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 
@@ -14,5 +15,12 @@ var version = "dev"
 
 func main() {
 	ctx := context.Background()
-	config.RunConnector(ctx, "baton-github-enterprise", version, cfg.Config, connector.NewLambdaConnector, connectorrunner.WithSessionStoreEnabled())
+	config.RunConnector(ctx,
+		"baton-github-enterprise",
+		version,
+		cfg.Config,
+		connector.NewLambdaConnector,
+		connectorrunner.WithSessionStoreEnabled(),
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilderV2(&githubConnector.GitHub{}),
+	)
 }
