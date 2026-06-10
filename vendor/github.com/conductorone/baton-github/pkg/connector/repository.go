4ccbb8b87e79c88abd6b9e2b69efeeecf22c6d60
Lauren Leach <lauren.leach@conductorone.com>
2026-06-10 12:38:19 -0700

    bump baton-github to v0.3.10, call app builder in default capabilites builder

--- vendor/github.com/conductorone/baton-github/pkg/connector/repository.go
+++ vendor/github.com/conductorone/baton-github/pkg/connector/repository.go
@@ -97,12 +97,7 @@ func (o *repositoryResourceType) List(ctx context.Context, parentID *v2.Resource
 		return nil, nil, wrapGitHubError(err, resp, "github-connector: failed to list repositories")
 	}
 
-	nextPage, reqAnnos, err := parseResp(resp)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	pageToken, err := bag.NextToken(nextPage)
+	pageToken, reqAnnos, err := nextPageToken(bag, resp)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -131,11 +126,19 @@ func (o *repositoryResourceType) Entitlements(_ context.Context, resource *v2.Re
 
 func (o *repositoryResourceType) StaticEntitlements(_ context.Context, _ resourceSdk.SyncOpAttrs) ([]*v2.Entitlement, *resourceSdk.SyncOpResults, error) {
 	rv := make([]*v2.Entitlement, 0, len(repoAccessLevels))
-	for _, level := range repoAccessLevels {
-		rv = append(rv, entitlement.NewPermissionEntitlement(nil, level,
+	for i, level := range repoAccessLevels {
+		rv = append(rv, entitlement.NewPermissionEntitlement(
+			nil,
+			level,
 			entitlement.WithDisplayName(fmt.Sprintf("Repo %s", titleCase(level))),
 			entitlement.WithDescription(fmt.Sprintf("Access to repository in GitHub as %s", level)),
 			entitlement.WithGrantableTo(resourceTypeUser, resourceTypeTeam),
+			entitlement.WithAnnotation(&v2.EntitlementExclusionGroup{
+				ExclusionGroupId: "repository",
+				Order:            uint32(i),
+				IsDefault:        false,
+				ScopeToResource:  true,
+			}),
 		))
 	}
 
